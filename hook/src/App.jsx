bc0cdaf392aedbe53982035ad3eb34a4d0ee210d
kiyongee2 <tje0714@outlook.kr>
2026-03-25 21:55:32 +0900

    3. 25 수업

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -10,33 +10,16 @@ import AddDrink from './components/AddDrink'
 import User from './components/User'
 import Clock from './components/Clock'
 import Profile from './card/Profile'
+import MapTest from './components/MapTest'
+import SignUp from './form/SignUp'
+import SignIn from './form/SignIn'
 
 function App() {
 
   return (
     <>
-      <Profile />
-      {/* <Clock /> */}
-      {/* <User /> */}
-      {/* <AddDrink /> */}
-
-    {/* <Box></Box> 사이의 모든 것이 {children}으로 전달 */}
-      {/* <Box>
-        <h3>박스 안의 내용</h3>
-        <p>이것은 Box 컴포넌트 안에 있는 내용입니다.</p>
-      </Box>
-      <Box>
-        <h3>또 다른 박스</h3>
-        <p>이것은 또 다른 Box 컴포넌트 안에 있는 내용입니다.</p>
-      </Box> */}
-
-      {/* <AddProduct /> */}
-
-      {/* <Car /> */}
-      {/* <Counter /> */}
-      {/* <Garden /> */}
-      {/* <Dog breed = "말티즈" age = {3} />
-      <Dog breed = "진돗개" age = {4} /> */}
+     <SignIn />
+     {/* <SignUp /> */}
     </>
   )
 }
