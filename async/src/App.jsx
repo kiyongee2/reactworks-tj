cbd65bf6e6ad695148776ef17c1c588fda5a456f
kiyongee2 <tje0714@outlook.kr>
2026-03-31 21:51:52 +0900

    style, async

---
+++ async/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import FetchExample from './components/FetchExample'
+
+function App() {
+
+  return (
+    <>
+      <section className="center">
+        <FetchExample />
+      </section>
+    </>
+  )
+}
+
+export default App
